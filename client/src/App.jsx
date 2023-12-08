2131c788dd5c75f2b566447d0725ef59a67e4e01
Sardar Tariq Aziz <9874sardar@gmail.com>
2023-12-08 19:39:50 +0530

    inital code written
    all inital details before writing code

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Chat from "@/components/chat"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+      <h1>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* <Navigate/> */}
+          <Route path="/chat" element={<Chat/>} />
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
+  )
+}
+
+export default App
